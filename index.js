14824fc9bfe348fbe57e5f0288077a6bd0941f13
Brijendra-Singh2003 <brijendra0369@gmail.com>
2023-03-26 14:51:10 +0530

    oh

--- index.js
+++ index.js
@@ -3,15 +3,13 @@ let b, d;
 function clicked(a, c) {
     if(b == null) { }
     else {
-        d.style.scale = "1";
         b.style.opacity = "0";
+        d.classList.toggle("tap");
     }
     x = Math.floor(Math.random() * 3);
     b = document.getElementById(x);
     b.style.opacity = "1";
-    // c.style.animationName = "go";
-    // c.style.animationDuration = "300ms";
-    c.style.scale = "0.9";
+    c.classList.toggle("tap");
     d = c;
-    clearInterval()
+    clearInterval(0.3)
 }
